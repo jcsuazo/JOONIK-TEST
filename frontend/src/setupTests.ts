76d8d1aa8c72f2f621635dac6a37c2f4a3047c32
Juan carlos Suazo dirocie <juancarlossuazodirocie@Juans-MacBook-Pro.local>
2025-01-24 12:48:42 -0400

    added frontend tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { server } from './mocks/server';
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished
+afterAll(() => server.close());
