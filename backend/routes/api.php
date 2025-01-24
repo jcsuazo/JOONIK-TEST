f89fa704888311007156ff3fe9c69bec4fce06fa
Juan carlos Suazo dirocie <juancarlossuazodirocie@Juans-MacBook-Pro.local>
2025-01-24 09:32:43 -0400

    added the locations route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get("/", function () {
-    return response()->json(["message" => "Hello World!"], 200);
-});
+Route::get("/locations", LocationController::class . "@index");
